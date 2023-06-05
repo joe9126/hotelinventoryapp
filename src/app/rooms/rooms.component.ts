ec932368426a155545c17ee5631a2a0a2e46e71f
joe9126 <joeasewe@gmail.com>
2023-06-05 14:35:01 +0300

    rooms component

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { AppComponent } from '../app.component';
+
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css']
 })
 export class RoomsComponent {
+  numberofRooms: number = 120;
+  availableRooms: number = 105;
+  bookedRooms: number = 15;
 
+  hideRooms : boolean = false;
+
+  app = new AppComponent();
+
+  hotelname = this.app.hotelname;
+
+  constructor(){}
+
+  ngOnInit(): void{}
+
+  toggle(){
+    this.hideRooms = !this.hideRooms;
+  }
 }
